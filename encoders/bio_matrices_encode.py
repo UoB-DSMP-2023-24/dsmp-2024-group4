bfa7ddbd3036e7af2bd214bf371384c03dba3798
xujiadong <piduoxu@qq.com>
2024-03-03 23:16:31 +0000

    Added the types of matrices that can be used. Improved the functions that can be called.

--- encoders/bio_matrices_encode.py
+++ encoders/bio_matrices_encode.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 import numpy as np
 import pandas as pd
 
-def make_aa_df():
+def BLOSUM90():
     # load matrix as symmetric pandas dataframe
     blosum100 = substitution_matrices.load("BLOSUM90")
     # Extract row and column names (unique amino acids)
@@ -26,10 +26,55 @@ def make_aa_df():
 
     return aa_df
 
-def encode_cdr3(seq,aa_idx,maxlength):
-    seq_embed = [aa_idx[aa].tolist() for aa in seq]
+def BLOSUM62():
+    blosum100 = substitution_matrices.load("BLOSUM62")
+    amino_acids = sorted(set(k[0] for k in blosum100.keys()).union(set(k[1] for k in blosum100.keys())))
+
+    aa_df = pd.DataFrame(index=amino_acids, columns=amino_acids)
+    for (aa1, aa2), score in blosum100.items():
+        aa_df.at[aa1, aa2] = score
+        aa_df.at[aa2, aa1] = score  # Ensure symmetry
+    standard_protein_letters = "IRQCYMLVAFNESHKWGDTP"
+    aa_df = aa_df.loc[list(standard_protein_letters), list(standard_protein_letters)]
+
+    # add NULL '-' with max loss (min value) & median for match
+    aa_df.loc['-', :] = aa_df.values.min()
+    aa_df.loc[:, '-'] = aa_df.values.min()
+    aa_df.loc['-', '-'] = np.median(aa_df.values.diagonal())
+
+    return aa_df
+
+def BLOSUM80():
+    blosum100 = substitution_matrices.load("BLOSUM80")
+    amino_acids = sorted(set(k[0] for k in blosum100.keys()).union(set(k[1] for k in blosum100.keys())))
+
+    aa_df = pd.DataFrame(index=amino_acids, columns=amino_acids)
+    for (aa1, aa2), score in blosum100.items():
+        aa_df.at[aa1, aa2] = score
+        aa_df.at[aa2, aa1] = score  # Ensure symmetry
+    standard_protein_letters = "IRQCYMLVAFNESHKWGDTP"
+    aa_df = aa_df.loc[list(standard_protein_letters), list(standard_protein_letters)]
+
+    # add NULL '-' with max loss (min value) & median for match
+    aa_df.loc['-', :] = aa_df.values.min()
+    aa_df.loc[:, '-'] = aa_df.values.min()
+    aa_df.loc['-', '-'] = np.median(aa_df.values.diagonal())
+
+    return aa_df
+
+
+def encode_cdr3(seq,bio_matrix_name,maxlength):
+    if bio_matrix_name == 'BLOSUM90':
+        bio_matrix = BLOSUM90()
+    elif bio_matrix_name == 'BLOSUM62':
+        bio_matrix = BLOSUM62()
+    elif bio_matrix_name == 'BLOSUM80':
+        bio_matrix = BLOSUM80()
+    else:
+        raise ValueError('No such matrix')
+    seq_embed = [bio_matrix[aa].tolist() for aa in seq]
     if len(seq_embed) < maxlength:
-        pad = [aa_idx['-'].tolist() for _ in range(maxlength - len(seq_embed))]
+        pad = [bio_matrix['-'].tolist() for _ in range(maxlength - len(seq_embed))]
         seq_embed.extend(pad)
     elif len(seq_embed) > maxlength:
         seq_embed = seq_embed[:maxlength]
@@ -42,7 +87,7 @@ epitope=df[9].tolist()
 # delete the first row('cdr3')
 cdr3.pop(0)
 epitope.pop(0)
-aa_idx = make_aa_df()
+aa_idx = BLOSUM90()
 encode_output= []
 for i in cdr3:
     print(encode_cdr3(i,aa_idx,20))
