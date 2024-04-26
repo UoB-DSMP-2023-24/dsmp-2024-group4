727107ea2bea26ca7ada9b4e954536d04c942b6f
xujiadong <piduoxu@qq.com>
2024-04-26 19:29:16 +0100

    GIANA_encoder.py creates functions that call GIANA's encoder methods to encode CDR3 sequences for pandas objects and "TCR" objects. umap_original_data.py compares the performance of UMAP dimensionality reduction using the encoded CDR3 and distance matrices.

--- encoders/GIANA_encoder.py
+++ encoders/GIANA_encoder.py
@@ -1,2 +1,46 @@
+from encoders.GIANA.GIANA4_1 import EncodingCDR3,M6,n0
+import numpy as np
+
+def GIANA_encoder(TCRs, ST=3):
+    cdr3_alpha_encoded = None
+    cdr3_beta_encoded = None
+    if TCRs[0].cdr3_alpha is not None:
+        cdr3_alpha = [tcr.cdr3_alpha for tcr in TCRs]
+        cdr3_alpha_encoded = [EncodingCDR3(seq[ST:-2], M6, n0) for seq in cdr3_alpha]
+        cdr3_alpha_array = np.array(cdr3_alpha_encoded)
+
+    if TCRs[0].cdr3_beta is not None:
+        cdr3_beta = [tcr.cdr3_beta for tcr in TCRs]
+        cdr3_beta_encoded = [EncodingCDR3(seq[ST:-2], M6, n0) for seq in cdr3_beta]
+        cdr3_beta_array = np.array(cdr3_beta_encoded)
+    if cdr3_alpha_encoded is not None and cdr3_beta_encoded is not None:
+        return np.concatenate((cdr3_alpha_array, cdr3_beta_array), axis=1) # concatenate alpha and beta chain encodings
+    elif cdr3_alpha_encoded is not None:
+        return cdr3_alpha_encoded
+    elif cdr3_beta_encoded is not None:
+        return cdr3_beta_encoded
+
+def GIANA_encoder_pd(df, chains=['alpha', 'beta'],ST=3):
+    if chains== ['alpha', 'beta']:
+        cdr3_alpha = df['cdr3_a_aa'].tolist()
+        cdr3_beta = df['cdr3_b_aa'].tolist()
+        cdr3_alpha_encoded = [EncodingCDR3(seq[ST:-2], M6, n0) for seq in cdr3_alpha]
+        cdr3_beta_encoded = [EncodingCDR3(seq[ST:-2], M6, n0) for seq in cdr3_beta]
+        cdr3_alpha_array = np.array(cdr3_alpha_encoded)
+        cdr3_beta_array = np.array(cdr3_beta_encoded)
+        cdr3_output=np.concatenate((cdr3_alpha_array, cdr3_beta_array), axis=1).tolist()
+        df.loc[:, 'encoded_cdr3'] = cdr3_output
+    elif chains== ['alpha']:
+        cdr3_alpha = df['cdr3_a_aa'].tolist()
+        cdr3_alpha_encoded = [EncodingCDR3(seq[ST:-2], M6, n0) for seq in cdr3_alpha]
+        df.loc[:, 'encoded_cdr3'] = cdr3_alpha_encoded
+    elif chains== ['beta']:
+        cdr3_beta = df['cdr3_b_aa'].tolist()
+        cdr3_beta_encoded = [EncodingCDR3(seq[ST:-2], M6, n0) for seq in cdr3_beta]
+        df.loc[:, 'encoded_cdr3'] = cdr3_beta_encoded
+    return df
+
+# human = GIADA_encoder_pd(human, chains=['alpha', 'beta'])
+# it will add a new column 'encoded_cdr3' to the dataframe, which contains the encoded cdr3 sequences.
 
 
